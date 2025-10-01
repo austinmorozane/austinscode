d8a5aed55a879f47fe18f59b62b356329979b789
austin <lab@mac.local>
2025-10-01 05:48:45 -0600

    cache fix

--- generate-cache.js
+++ generate-cache.js
@@ -2,20 +2,17 @@ const fs = require("fs");
 const path = require("path");
 const crypto = require("crypto");
 
-// Configuration
-const imageDir = path.join(__dirname, "build/fast_imgs"); // Ensure images are copied to
+const imageDir = path.join(__dirname, "build/fast_imgs"); // Ensure images are copied to build
 const buildCssDir = path.join(__dirname, "build/static/css"); // React build CSS path
 const outputFile = path.join(__dirname, "public/assets-to-cache.json");
 
 function getFilesWithHash(directory, basePath) {
   return fs.existsSync(directory)
     ? fs.readdirSync(directory)
-        .filter(file => !file === '.DS_Store' &&
-/\.(css|css\.map|png|jpg|jpeg|svg|gif|webp)$/i.test(file)) // Include images & CSS
+        .filter(file => /\.(css|css\.map|png|jpg|jpeg|svg|gif|webp)$/i.test(file)) // Include images & CSS
         .map((file) => {
           const filePath = path.join(directory, file);
-          const hash =
-crypto.createHash("md5").update(fs.readFileSync(filePath)).digest("hex").slice(0, 8);
+          const hash = crypto.createHash("md5").update(fs.readFileSync(filePath)).digest("hex").slice(0, 8);
           return `/${basePath}/${file}?v=${hash}`;
         })
     : [];
@@ -24,8 +21,7 @@ crypto.createHash("md5").update(fs.readFileSync(filePath)).digest("hex").slice(0
 // Get images and CSS (including .map files) from the React build
 const images = getFilesWithHash(imageDir, "fast_imgs");
 console.log("Checking Images Directory:", imageDir);
-console.log("Files Found:", fs.existsSync(imageDir) ? fs.readdirSync(imageDir).filter(file =>
-file !== '.DS_Store') : "Directory Not Found");
+console.log("Files Found:", fs.existsSync(imageDir) ? fs.readdirSync(imageDir) : "Directory Not Found");
 const cssFiles = getFilesWithHash(buildCssDir, "static/css");
 const assetsToCache = [...images, ...cssFiles];
 
